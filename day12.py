b50413ae12b2b0f1964b3bcdd0ca60eefd8c0790
banset <mohitbasnet80@gmail.com>
2024-03-28 19:00:10 +0545

    method overiding

---
+++ day12.py
@@ -0,0 +1,18 @@
+# Method Overriding
+class Phone:
+    def __init__(self, price, brand, camera):
+        print ("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print ("Buying a phone")
+
+class SmartPhone(Phone):
+    def buy(self):
+        print ("Buying a smartphone")
+
+s=SmartPhone(20000, "Apple", 13)
+
+s.buy()
