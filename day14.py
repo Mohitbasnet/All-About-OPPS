286c3f4c02d512c7fdb896e1935d58991025c154
banset <mohitbasnet80@gmail.com>
2024-03-30 19:17:19 +0545

    added inheritance

---
+++ day14.py
@@ -0,0 +1,20 @@
+# super -> constuctor
+class Phone:
+    def __init__(self, price, brand, camera):
+        print ("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+class SmartPhone(Phone):
+    def __init__(self, price, brand, camera, os, ram):
+        print('Inside smartphone constructor')
+        super().__init__(price, brand, camera)
+        self.os = os
+        self.ram = ram
+        print ("Inside smartphone constructor")
+
+s=SmartPhone(20000, "Samsung", 12, "Android", 2)
+
+print(s.os)
+print(s.brand)
