c01311d24fd2a880b67984e889581e9dc35bebdd
banset <mohitbasnet80@gmail.com>
2024-03-31 12:24:59 +0545

    added day14

--- day14.py
+++ day14.py
@@ -18,3 +18,66 @@ s=SmartPhone(20000, "Samsung", 12, "Android", 2)
 
 print(s.os)
 print(s.brand)
+
+# Question 1
+class Parent:
+
+    def __init__(self,num):
+      self.__num=num
+
+    def get_num(self):
+      return self.__num
+
+class Child(Parent):
+  
+    def __init__(self,num,val):
+      super().__init__(num)
+      self.__val=val
+
+    def get_val(self):
+      return self.__val
+      
+son=Child(100,200)
+print(son.get_num())
+print(son.get_val())
+
+
+
+# question2
+
+class Parent:
+    def __init__(self):
+        self.num=100
+
+class Child(Parent):
+
+    def __init__(self):
+        super().__init__()
+        self.var=200
+        
+    def show(self):
+        print(self.num)
+        print(self.var)
+
+son=Child()
+son.show()
+
+
+# Question 3
+class Parent:
+    def __init__(self):
+        self.__num=100
+
+    def show(self):
+        print("Parent:",self.__num)
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        self.__var=10
+
+    def show(self):
+        print("Child:",self.__var)
+
+obj=Child()
+obj.show()
